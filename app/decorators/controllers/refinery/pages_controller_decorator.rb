0ebc9e5e1ae28ed66d81a56dd8b86862a8b96ca9
David Colucci <davidanthonycolucci@gmail.com>
2018-02-18 19:02:29 -0500

    fetch posts in reverse chron order

--- app/decorators/controllers/refinery/pages_controller_decorator.rb
+++ app/decorators/controllers/refinery/pages_controller_decorator.rb
@@ -2,6 +2,8 @@ Refinery::PagesController.class_eval do
   before_action :find_all_blog_posts, :only => [:home]
 
   def find_all_blog_posts
-    @blog_posts = Refinery::Page.where(view_template: 'post')
+    @blog_posts = Refinery::Page.where(view_template: 'post').sort do |a,b|
+      b.updated_at.to_date <=> a.updated_at.to_date
+    end
   end
 end
