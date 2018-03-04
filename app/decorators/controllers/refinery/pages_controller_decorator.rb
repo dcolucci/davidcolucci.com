f9046d23926100cfc8cd114f90bcbfc3f0df7db0
David Colucci <davidanthonycolucci@gmail.com>
2018-03-04 12:45:00 -0500

    render homepage canvas based on env variable

--- app/decorators/controllers/refinery/pages_controller_decorator.rb
+++ app/decorators/controllers/refinery/pages_controller_decorator.rb
@@ -1,9 +1,14 @@
 Refinery::PagesController.class_eval do
   before_action :find_all_blog_posts, :only => [:home]
+  before_action :should_render_canvas?, :only => [:home]
 
   def find_all_blog_posts
     @blog_posts = Refinery::Page.where(view_template: 'post').sort do |a,b|
       b.updated_at.to_date <=> a.updated_at.to_date
     end
+  end
+
+  def should_render_canvas?
+    @should_render_canvas = ENV['HOMEPAGE_CANVAS'] == 'true'
   end
 end
