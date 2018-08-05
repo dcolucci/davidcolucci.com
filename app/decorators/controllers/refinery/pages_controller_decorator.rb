c9efaa7777e572407dc7076b43a7b8bc2c541189
David Colucci <davidanthonycolucci@gmail.com>
2018-08-05 09:10:02 -0400

    use created_at instead of updated_at for timestamp and river retrieval

--- app/decorators/controllers/refinery/pages_controller_decorator.rb
+++ app/decorators/controllers/refinery/pages_controller_decorator.rb
@@ -4,7 +4,7 @@ Refinery::PagesController.class_eval do
 
   def find_all_blog_posts
     @blog_posts = Refinery::Page.where(view_template: 'post').sort do |a,b|
-      b.updated_at.to_date <=> a.updated_at.to_date
+      b.created_at.to_date <=> a.created_at.to_date
     end
   end
 
