d06936459f968918da7016895c437d7d209968c5
David Colucci <david_colucci@condenast.com>
2020-04-27 21:01:03 -0400

    adds support for aggregating child pages on post pages, and adds some navigation convenience

--- app/decorators/controllers/refinery/pages_controller_decorator.rb
+++ app/decorators/controllers/refinery/pages_controller_decorator.rb
@@ -1,6 +1,7 @@
 Refinery::PagesController.class_eval do
   before_action :find_all_blog_posts, :only => [:home]
   before_action :should_render_canvas?, :only => [:home]
+  before_action :render_children
 
   def find_all_blog_posts
     @blog_posts = Refinery::Page.where(view_template: 'post').sort do |a,b|
@@ -10,5 +11,15 @@ Refinery::PagesController.class_eval do
 
   def should_render_canvas?
     @should_render_canvas = ENV['HOMEPAGE_CANVAS'] == 'true'
+  end
+
+  def render_children
+    @children = @page.children.sort do |a,b|
+      b.created_at.to_date <=> a.created_at.to_date
+    end
+
+    if @page.parent_id != nil
+      @parent_page = Refinery::Page.find(@page.parent_id)
+    end
   end
 end
